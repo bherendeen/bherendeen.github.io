f2f5a42b564a9f33cdcca1cc4e0ed7af1e48ae95
bherendeen <64050150+bherendeen@users.noreply.github.com>
2020-05-29 09:32:56 -0600

    Update styles.js

--- lesson6/js/styles.js
+++ lesson6/js/styles.js
@@ -19,28 +19,3 @@ if (dayOfWeek == 5) {
 } else {
     document.getElementById("notice").style.display = 'none';
 }
-
-// Font
-// T = Temperature in degrees Fahrenheit | V = Wind velocity in miles per hour
-// Wind chill = 35.74 + 0.6215T – 35.75(V ^ 0.16) + 0.4275T(V ^ 0.16)
-
-let currentCondition = 'Sunny';
-let highTemp = 30;
-let lowTemp = 15;
-let windSpeed = 3;
-
-let windChill = windChillCalc(highTemp, windSpeed);
-
-document.getElementById('currentCondition').innerHTML = currentCondition;
-document.getElementById('highTemp').innerHTML = highTemp;
-document.getElementById('lowTemp').innerHTML = lowTemp;
-document.getElementById('windSpeed').innerHTML = windSpeed;
-document.getElementById('output').innerHTML = windChill;
-
-function windChillCalc(temp, windSpeed) {
-
-    let chillCalc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(windSpeed, .16) + .4275 * temp * Math.pow(windSpeed, .16);
-
-    return Math.round(chillCalc);
-
-}
