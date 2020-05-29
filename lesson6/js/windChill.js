6a2edcc3e37680caecee0b58fdfa4e6ffb7efc9c
bherendeen <64050150+bherendeen@users.noreply.github.com>
2020-05-29 09:22:25 -0600

    Update windChill.js

--- lesson6/js/windChill.js
+++ lesson6/js/windChill.js
@@ -10,7 +10,7 @@ function windChillCalc(temp, windSpeed) {
 
     if (temp <= 50 && windSpeed >= 3) {
         let chillCalc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(windSpeed, .16) + .4275 * temp * Math.pow(windSpeed, .16);
-        return Math.round(chillCalc * 100) / 100;
+        return Math.round(chillCalc * 10) / 10;
     } else {
         return 'N/A';
     }
