70484f195114cfc54246fc2b70f6e4f948aeda6e
bherendeen <64050150+bherendeen@users.noreply.github.com>
2020-06-25 17:19:05 -0600

    Update weatherapi.js

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -3,20 +3,14 @@ const apiURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&unit
 fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
-        //console.log(jsObject);
-
 
         document.getElementById('weatherDes').textContent = jsObject.list[0].weather[0].description;
         document.getElementById('current-temp').textContent = jsObject.list[6].main.temp.toFixed(0);
         document.getElementById('hum').textContent = jsObject.list[0].main.humidity;
         document.getElementById('windSpeed').textContent = jsObject.list[0].wind.speed;
 
-
         const forecast = jsObject.list.filter(x => x.dt_txt.includes("18:00:00"));
-
         const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-        console.log(forecast);
 
         let i = 0;
         forecast.forEach(fiveDayForecast => {
@@ -31,27 +25,3 @@ fetch(apiURL)
         });
 
     });
-/*
-        for (let i = 0; i < forecast.length; i++) {
-            document.getElementById(`temp${i + 1}`).textContent = `${forecast[i].main.temp.toFixed(0)}F`;
-            const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather.icon + '.png';
-            const desc = jsObject.weather[0].description;
-
-            document.getElementById('imagesrc').textContent = imagesrc;
-            document.getElementById(`icon${i + 1}`).setAttribute('src', imagesrc);
-            document.getElementById(`icon${i + 1}`).setAttribute('alt', desc);
-
-        }
-    });
-    */
-/*
-function getDays(day) {
-    let date = new Date();
-    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    let dayOfWeek = date.getDay();
-
-    dayOfWeek += day;
-
-    return days[dayOfWeek];
-}
-*/
